5584fdfa033d59983b5fb64aadeea1d305d8bfa8
zijian.cheng <mr.chengzj@gmail.com>
2018-02-28 14:57:31 +0800

    [fixed] 修改一些问题

--- wx/hongbao.py
+++ wx/hongbao.py
@@ -12,6 +12,7 @@ sys.setdefaultencoding('utf-8')
 # 扫码登录
 bot = Bot(cache_path=True)
 
+
 """
 默认消息处理方法
 ==============================================================================
@@ -33,69 +34,71 @@ def _get_red_pack(user, link, mobile):
 
 
 def _default_text_register(text_msg):
-    print str(text_msg.sender.name)
-    my_friends = bot.friends().search(str(text_msg.sender.name))
+    my_friends = bot.friends().search(text_msg.sender.name)
     for user in my_friends:
         user.mark_as_read()
-        if text_msg.text == 'Q':
-            if not str(user.remark_name):
-                user.send('您还未绑定手机号'.decode("UTF-8"))
-            else:
-                remark_name = re.findall(r"^[1][3,4,5,6,7,8][0-9]{9}$", str(user.remark_name))
-                if remark_name:
-                    user.send(('当前绑定的手机号码为：%s' % str(user.remark_name)).decode("UTF-8"))
-                else:
-                    user.send('绑定的号码有误，请正确绑定'.decode("UTF-8"))
-        elif text_msg.text == 'H':
-            user.send('******使用说明******\n'
-                      '首次使用需绑定手机号码\n'
-                      '*绑定方法*\n'
-                      '**直接发送手机号码给我\n\n'
-                      '*领取红包方法*\n'
-                      '**直接将外卖红包分享给我\n'
-                      '**或发送外卖红包链接地址'.decode("UTF-8"))
-        else:
-            new_remark_name = re.findall(r"^[1][3,4,5,6,7,8][0-9]{9}$", text_msg.text)
-            if new_remark_name:
-                new_remark_name = '' + str(new_remark_name).replace('[u\'', '').replace('\']', '')
-                user.set_remark_name(new_remark_name)
-                user.send(('成功将手机号码绑定为：%s' % str(new_remark_name)).decode("UTF-8"))
-            elif 'h5.ele.me/hongbao' in text_msg.text or 'activity.waimai.meituan.com' in text_msg.text:
-                if not user.remark_name:
+        # 避免发送到别的用户身上
+        if str(text_msg.sender.name) == str(user.name):
+            if text_msg.text == 'Q' or text_msg.text == 'q':
+                if not str(user.remark_name):
                     user.send('您还未绑定手机号'.decode("UTF-8"))
                 else:
-                    remark_name = re.findall(r"^[1][3,4,5,6,7,8][0-9]{9}$", user.remark_name)
+                    remark_name = re.findall(r"^[1][3,4,5,6,7,8][0-9]{9}$", str(user.remark_name))
                     if remark_name:
-                        user.send('红包领取中…'.decode("UTF-8"))
-                        _get_red_pack(user, text_msg.text, remark_name)
+                        user.send(('当前绑定的手机号码为：%s' % str(user.remark_name)).decode("UTF-8"))
                     else:
                         user.send('绑定的号码有误，请正确绑定'.decode("UTF-8"))
-
+            elif text_msg.text == 'H' or text_msg.text == 'h':
+                user.send('******使用说明******\n'
+                          '首次使用需绑定手机号码\n'
+                          '*绑定方法*\n'
+                          '**直接发送手机号码给我\n\n'
+                          '*领取红包方法*\n'
+                          '**直接将外卖红包分享给我\n'
+                          '**或发送外卖红包链接地址'.decode("UTF-8"))
             else:
-                user.send('我是机器人，你说的啥我不懂'.decode("UTF-8"))
-        break
+                new_remark_name = re.findall(r"^[1][3,4,5,6,7,8][0-9]{9}$", text_msg.text)
+                if new_remark_name:
+                    new_remark_name = '' + str(new_remark_name).replace('[u\'', '').replace('\']', '')
+                    user.set_remark_name(new_remark_name)
+                    user.send(('成功将手机号码绑定为：%s' % str(new_remark_name)).decode("UTF-8"))
+                elif 'h5.ele.me/hongbao' in text_msg.text or 'activity.waimai.meituan.com' in text_msg.text:
+                    if not user.remark_name:
+                        user.send('您还未绑定手机号'.decode("UTF-8"))
+                    else:
+                        remark_name = re.findall(r"^[1][3,4,5,6,7,8][0-9]{9}$", user.remark_name)
+                        if remark_name:
+                            user.send('红包领取中…'.decode("UTF-8"))
+                            _get_red_pack(user, text_msg.text, remark_name)
+                        else:
+                            user.send('绑定的号码有误，请正确绑定'.decode("UTF-8"))
+
+                else:
+                    user.send('我是机器人，你说的啥我不懂'.decode("UTF-8"))
+            break
 
 
 def _default_share_register(share_msg):
     my_friends = bot.friends().search(share_msg.sender.name)
     for user in my_friends:
         user.mark_as_read()
-        if user.remark_name:
-            remark_name = re.findall(r"^[1][3,4,5,6,7,8][0-9]{9}$", user.remark_name)
-            if remark_name:
-                mobile = '' + str(remark_name).replace('[u\'', '').replace('\']', '')
-                url = share_msg.raw["Url"]
-                url = str(url).replace("&amp;", "&")
-                # print url
-                # print mobile
-                user.send('红包领取中…'.decode("UTF-8"))
-                _get_red_pack(user, url, mobile)
+        if str(share_msg.sender.name) == str(user.name):
+            if user.remark_name:
+                remark_name = re.findall(r"^[1][3,4,5,6,7,8][0-9]{9}$", user.remark_name)
+                if remark_name:
+                    mobile = '' + str(remark_name).replace('[u\'', '').replace('\']', '')
+                    url = share_msg.raw["Url"]
+                    url = str(url).replace("&amp;", "&")
+                    # print url
+                    # print mobile
+                    user.send('红包领取中…'.decode("UTF-8"))
+                    _get_red_pack(user, url, mobile)
+                else:
+                    user.send('领取红包前请先绑定要领取的手机号（直接发送号码即可）'.decode("UTF-8"))
             else:
                 user.send('领取红包前请先绑定要领取的手机号（直接发送号码即可）'.decode("UTF-8"))
-        else:
-            user.send('领取红包前请先绑定要领取的手机号（直接发送号码即可）'.decode("UTF-8"))
 
-        break
+            break
 
 
 @bot.register()
